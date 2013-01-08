2da6e3e486fcb33fceb49ab20d38a68627191506
Sarah Jane Aristain <sarah@klab.com>
2013-01-08 12:06:06 +0800

    Add changes to webpage

--- sample.js
+++ sample.js
@@ -2,18 +2,12 @@ var Sample = {
 
     initView: function(){
         //box click
-        $('#the_box').click(function(){
-            var pos = $(this).position();
-            var leftPos = pos.left;
-
-            $(this).css({ left: leftPos+1 });
-        });
 
         //add row
         var rowCtr = 1;
         $('#addRow').click(function(){
             var tds = "";
-            $('#ths th').each(function(){
+            $('#ths td').each(function(){
                 if($(this).html() > 0){
                     tds += '<td>'+ rowCtr * $(this).html() +'</td>';
                 }
@@ -26,10 +20,19 @@ var Sample = {
         });
 
         //add col
-        var colCtr = 1;
+        var colCtr = 2;
         $('#addCol').click(function(){
-            //$('<th>'+ colCtr++ +'</td>').appendTo($('#ths'));
-            alert("Computation for column not yet done");
+            $('#tbl tr').each(function(){
+                console.log($($(this).find('td:first')).text());
+                var b = parseInt($($(this).find('td:first')).text());
+                if(b){
+                    $('<td>'+ colCtr * b +'</td>').appendTo($(this));
+                }
+                else{
+                    $('<td>'+ colCtr +'</td>').appendTo($(this));
+                }
+            });
+            colCtr++
         });
     }
 };
